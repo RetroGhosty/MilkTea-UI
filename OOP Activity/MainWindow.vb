1949d2eec77283cc738b69daf564d2d77f87aea0
rhianneskie <rhianneangelramos@gmail.com>
2023-05-18 13:08:23 +0800

    Squashed commit of the following:
    commit c6dd796f18bf1ddac66a6319535a964abdb3061e
    Author: talipapa <54449218+RetroGhosty@users.noreply.github.com>
    Date:   Thu May 18 13:07:14 2023 +0800
    
        test
    
    commit 38132a5ec838bbe50a038f64c88be04c5b61c6ad
    Author: talipapa <54449218+RetroGhosty@users.noreply.github.com>
    Date:   Thu May 18 13:06:36 2023 +0800
    
        Squashed commit of the following:
    
        commit 654c8a38791186abf5427fea6ac98000eef6f599
        Author: talipapa <54449218+RetroGhosty@users.noreply.github.com>
        Date:   Thu May 18 12:28:46 2023 +0800
    
            added order view model
    
        commit ea1e4548bfe089b1c3a33de30d37d64d3559aa04
        Author: talipapa <54449218+RetroGhosty@users.noreply.github.com>
        Date:   Thu May 18 12:24:52 2023 +0800
    
            Added Order View Model
    
        commit 5f5d26d2649ccc1a8105ab52280896f887ce61e2
        Author: talipapa <54449218+RetroGhosty@users.noreply.github.com>
        Date:   Thu May 18 10:36:39 2023 +0800
    
            test start1

--- OOP Activity/MainWindow.vb
+++ OOP Activity/MainWindow.vb
@@ -1,3 +1,6 @@
-﻿Public Class MainWindow
-
+﻿
+Public Class MainWindow
+    Dim broTest As ItemStorage = New ItemStorage()
+    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
 End Class
